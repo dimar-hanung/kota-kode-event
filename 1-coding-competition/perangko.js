83c33a4564b1e6762d9a3e3cd17d4b6de1c9c62c
dimarhanung <dimarhanung@ecampus.ut.ac.id>
2021-03-24 23:06:14 +0700

    refactor: nama variable

--- 1-coding-competition/perangko.js
+++ 1-coding-competition/perangko.js
@@ -8,15 +8,13 @@
 
 function kasihPerangko(arr1) {
   //kode kamu di sini
-  const arr1Const = arr1.slice();
+  const arr1Ori = arr1.slice();
   arr1.forEach((item, i) => {
-    if (item > arr1Const[i - 1]) {
+    if (item > arr1Ori[i - 1]) {
       arr1[i] = arr1[i - 1] + 1;
-    } else if (item > arr1Const[i + 1]) {
+    } else if (item > arr1Ori[i + 1]) {
       //   arr1[i + 1] = arr1[i] - 1;
       arr1[i] = 2;
-
-      console.log(item, arr1[i + 1]);
     } else {
       arr1[i] = 1;
     }
@@ -24,8 +22,8 @@ function kasihPerangko(arr1) {
   console.log(arr1);
   return arr1.reduce((a, b) => a + b);
 }
-const caseA = kasihPerangko([4, 6, 4, 5, 6, 2]);
-const caseB = kasihPerangko([2, 4, 2, 6, 3, 7, 8, 9, 2, 1]);
-const caseC = kasihPerangko([2, 4, 3, 5, 2, 6, 4, 5]);
+const kasusA = kasihPerangko([4, 6, 4, 5, 6, 2]);
+const kasusB = kasihPerangko([2, 4, 2, 6, 3, 7, 8, 9, 2, 1]);
+const kasusC = kasihPerangko([2, 4, 3, 5, 2, 6, 4, 5]);
 
-console.log({ caseA, caseB, caseC });
+console.log({ kasusA, kasusB, kasusC });
