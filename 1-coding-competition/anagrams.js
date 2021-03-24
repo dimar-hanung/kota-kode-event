e442b62ff52164e15f628302cbe4ba956c0e0542
dimarhanung <dimarhanung@ecampus.ut.ac.id>
2021-03-24 23:05:19 +0700

    docs: meningkatkan kejelasan

--- 1-coding-competition/anagrams.js
+++ 1-coding-competition/anagrams.js
@@ -12,22 +12,32 @@
  *  - `includes` polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes#polyfill
  */
 
+/*
+======================================================================================================
+=============================================== Code 1 ===============================================
+=====================================================================================================
+*/
 /**
- * Rekomendasi : ⭐⭐⭐
+ * Rekomendasi : ⭐⭐⭐⭐
  * @param {String} str1 - Text pertama
  * @param {String} str2 - Text kedua
+ * @description
+ * - Minimal : ECMAScript 2015 (es6) / Gunakan Polyfill
+ * - Menghitung jumlah huruf yang perlu
+ * dihapus supaya 2 buah string bisa menjadi sebuah anagram.
  */
 function membuatAnagram(str1, str2) {
   // kode kamu di sini
 
   /**
-   * Mendapatkan list huruf yang duplikat terlebih dahulu.
+   * - str1 difilter dengan kondisi benar jika str2 mengandung salah satu huruf di str1
+   * - dengan algoritma ini akan didapatkan list huruf yang terduplikat
    */
   const charDuplikat = str1.split("").filter(char => str2.includes(char));
 
   /**
    * Setelah itu gabung `str1` dan `str2`, dan filter kembali,
-   * karena yang dicari adalah anagram
+   * karena yang dicari adalah anagram,
    * maka yang harus difilter adalah jumlah huruf yang tidak terduplikasi
    */
   const output = (str1 + str2)
@@ -37,8 +47,7 @@ function membuatAnagram(str1, str2) {
   return output;
 }
 
-const kasusA = membuatAnagram("adkrn", "r");
-const kasusB = membuatAnagram("adkrn", "r");
-const kasusC = membuatAnagram("adkrn", "r");
-
+const kasusA = membuatAnagram("dce", "efc"); // 2
+const kasusB = membuatAnagram("cdfea", "afbbk"); // 6
+const kasusC = membuatAnagram("adkrn", "r"); // 4
 console.log({ kasusA, kasusB, kasusC });
